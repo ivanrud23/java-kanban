0ed6fed38d51c5fdbd812918121f6f55b7e6d35c
IN_RD <ivanrud23@yandex.ru>
2023-04-11 22:11:25 +0300

    edfsd

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,9 +1,7 @@
 package service;
 
 public class Managers {
-    public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
-    }
+
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
